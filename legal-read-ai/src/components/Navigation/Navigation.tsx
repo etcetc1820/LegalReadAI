e911b49b89c6861ef55dfe3670f4e047e6aee0a7
Ihor Naidonov <etcetc1820@gmail.com>
2023-03-29 12:27:25 +0300

    responsive nav

--- legal-read-ai/src/components/Navigation/Navigation.tsx
+++ legal-read-ai/src/components/Navigation/Navigation.tsx
@@ -1,36 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import twitterIcon from '../../assets/twitterIcon.svg';
 import linkedInIcon from '../../assets/linkedinIcon.svg';
-/* 
-document
-        .getElementById("menu-toggle")
-        .addEventListener("click", function () {
-          document.getElementById("menu-items").classList.toggle("hidden");
-        });
-*/
+
 export const Navigation: React.FC = () => {
+  const [isHiding, setIsHiding] = useState(true);
+
+  const toggleMenu = () => {
+    setIsHiding(!isHiding);
+  };
+
   return (
     <nav className="bg-gray-100 py-8 px-6 lg:flex justify-between scroll-navbar">
       <div className="flex items-center justify-between">
         <a className="text-primary text-3xl font-normal" href="/">
           LegalRead <i className="pr-4 fa-solid fa-book-open-reader"></i>
         </a>
-        <button id="menu-toggle" className="lg:hidden text-xl">
+        <button
+          id="menu-toggle"
+          className="lg:hidden text-xl"
+          onClick={toggleMenu}
+        >
           <i className="fas fa-bars text-primary"></i>
         </button>
       </div>
       <div className="flex flex-col justify-center">
         <div
           id="menu-items"
-          className="hidden flex space-y-6 nav flex-col mt-2 lg:mt-0 lg:flex lg:flex-row lg:space-y-0 lg:space-x-4 pt-6"
+          className={`${
+            isHiding ? 'hidden items-stretch' : ''
+          } flex space-y-6 nav flex-col mt-2 lg:mt-0 lg:flex lg:flex-row lg:space-y-0 lg:space-x-4 pt-6`}
         >
           <a href="/">
-            <button className="border border-gray-500 px-4 py-2 rounded-md text-grborder-gray-500">
+            <button className="w-full border border-gray-500 px-4 py-2 rounded-md text-grborder-gray-500">
               Home
             </button>
           </a>
           <a href="#trynow">
-            <button className="border border-gray-500 px-4 py-2 rounded-md text-grborder-gray-500">
+            <button className="w-full border border-gray-500 px-4 py-2 rounded-md text-grborder-gray-500">
               Try Now
             </button>
           </a>
@@ -39,7 +45,7 @@ export const Navigation: React.FC = () => {
             target="_blank"
             rel="noreferrer"
           >
-            <button className="border border-gray-500 px-4 py-2 rounded-md text-grborder-gray-500">
+            <button className="w-full border border-gray-500 px-4 py-2 rounded-md text-grborder-gray-500">
               Join Waitlist
             </button>
           </a>
